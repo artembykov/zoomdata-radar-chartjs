cc4efdf57f21d7732c37439d41a117d0dd15582f
Artem Bykov <artemio91@gmail.com>
2018-11-13 15:47:45 +0200

    Remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,8 @@
 module.exports = {
+    entry: './src/index.js',
     output: {
         filename: 'visualization.js',
         path: __dirname
     },
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    cacheDirectory: true,
-                }
-            }
-        ]
-    }
+    mode: 'production'
 };
